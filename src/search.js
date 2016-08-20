e9f8fb58c574d1e4e08c177aba36ff3493c1c718
platonev <platonewang@gmail.com>
2016-08-20 11:08:31 +0800

    add suggest as typing in search box

--- src/search.js
+++ src/search.js
@@ -18,9 +18,39 @@ function search(word) {
     xhr.send();
 }
 
+function suggest() {
+    var q = document.getElementById('search_input').value;
+    var suggest = document.getElementById('suggest_list');
+
+    if (q.length > 0) {
+        var request = new XMLHttpRequest();
+        request.open('GET', 'http://dict.youdao.com/suggest?type=DESKDICT&num=5&q=' + q);
+        request.onreadystatechange = function () {
+            if (request.readyState === XMLHttpRequest.DONE && request.status === 200) {
+
+                suggest.innerHTML = '';
+
+                var responseXML = request.responseXML;
+
+                var items = responseXML.getElementsByTagName('item');
+
+                for (var i = 0; i < items.length; i++) {
+
+                    suggest.innerHTML += '<li class="item">' + items[i].getElementsByTagName("title")[0].firstChild.nodeValue + '</li>'
+                }
+            }
+        }
+        request.send();
+    } else {
+        suggest.innerHTML = '';
+    }
+}
+
 document.getElementById('search_input').onkeyup = function (e) {
     if (e.keyCode == 13) {
-
+        document.getElementById('suggest_list').innerHTML = '';
         search(document.getElementById('search_input').value);
+    } else {
+        suggest();
     }
 };
