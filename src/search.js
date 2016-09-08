3c397d00911e7ec254c20d9ccfd61c0dd473fafc
platonev <platonewang@gmail.com>
2016-09-08 08:44:16 +0800

    add phonetic

--- src/search.js
+++ src/search.js
@@ -1,4 +1,32 @@
 function search(word) {
+    var phoneticRequest = new XMLHttpRequest();
+    phoneticRequest.open('GET', 'http://m.youdao.com/dict?le=eng&q=' + word);
+    phoneticRequest.onreadystatechange = function () {
+        if (phoneticRequest.readyState === XMLHttpRequest.DONE && phoneticRequest.status === 200) {
+
+            var parser = new DOMParser();
+            var res = parser.parseFromString(phoneticRequest.responseText, 'text/html');
+            var items = res.getElementsByTagName('span');
+            var english, america;
+            var hasEnglish, hasAmerica;
+
+            if (phoneticRequest.responseText)
+
+            for (var i = 0; i < items.length; i++) {
+                if (items[i].getAttribute('class') == 'phonetic') {
+                    if (english === undefined) {
+                        english = items[i].firstChild.nodeValue;
+                    } else {
+                        america = items[i].firstChild.nodeValue;
+                    }
+                }
+            }
+
+            document.getElementById('phonetic').innerHTML = '<span>英</span><span class="phonetic" id="english">' + english + '</span><span>美</span><span class="phonetic" id="america">' + america + '</span>';
+        }
+    }
+    phoneticRequest.send();
+
     var request = new XMLHttpRequest();
     request.open('GET', 'http://m.youdao.com/singledict?q=' + word + '&dict=ee&le=eng&more=false');
     request.onreadystatechange = function () {
